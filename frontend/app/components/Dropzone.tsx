da46def4dd093ca19fb9c3d39fd0a18e8061275d
Samashi11 <maulashidqic10@gmail.com>
2026-06-26 19:36:52 +0700

    Update api/documents

--- frontend/app/components/Dropzone.tsx
+++ frontend/app/components/Dropzone.tsx
@@ -18,7 +18,7 @@ export default function Dropzone() {
     setSuccess(false);
 
     try {
-      const res = await fetch("https://fullstackprogrammerdeadline-back-production.up.railway.app//api/documents/upload", {
+      const res = await fetch("https://fullstackprogrammerdeadline-back-production.up.railway.app/api/documents/upload", {
         method: "POST",
         body: formData,
       });
